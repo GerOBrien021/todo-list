77b77c553b5a6b65925cee8ac776a292916c5639
GerOBrien021 <gerard.obrien.cork@gmail.com>
2025-12-05 07:53:00 -0500

    add ToDoList state variables and event handlers

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -30,6 +30,16 @@ const TodoList = () => {
             </div>
         </div>
         <div className="todo_main">
+            {todos.map((todo, index) => ( // Iterate over each todo item in the todos array
+                <div key={index} className="todo-card">
+                    <div className="heading_todo">
+                        {/* Display the heading text of the current todo item */}
+                        <h3>{todo.heading}</h3> {/* Display the heading here */}
+                        {/* Button to delete the current heading by passing its index */}
+                        <button className="delete-button-heading" onClick={() => handleDeleteTodo(index)}>Delete Heading </button>
+                    </div>
+                </div>
+            ))}
             
         </div>
         </>
