252f88ca5a5449f226ee712cc2658e4a57e7f866
GerOBrien021 <gerard.obrien.cork@gmail.com>
2025-12-05 07:35:45 -0500

    add ToDoList state variables and event handlers

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,27 +2,38 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
-  
+    const [todos, setTodos] = useState([]);
+    const [headingInput, setHeadingInput] = useState('');
+    const [listInputs, setListInputs] = useState({});
 
-  return (
-    <>
-      <div className="todo-container">
-        <h1 className="title">My Todo List</h1>
-        <div className="input-container">
-          <input
-            type="text"
-            className="heading-input"
-            placeholder="Enter heading"
-            
-          />
-          <button className="add-list-button">Add Heading</button>
+    const handleAddTodo = () => {
+        if (headingInput.trim() !== '') {
+          setTodos([...todos, { heading: headingInput, lists: [] }]);
+          setHeadingInput('');
+        }
+      };
+
+    return (
+        <>
+        <div className="todo-container">
+            <h1 className="title">My Todo List</h1>
+            <div className="input-container">{/* Input field to enter a new heading */}
+                <input
+                    type="text"
+                    className="heading-input"// CSS class for styling
+                    placeholder="Enter heading"// Text shown when input is empty
+                    value={headingInput}
+                    onChange={(e) => {setHeadingInput(e.target.value);}} // Add onChange event handler to update headingInput state
+                />
+                {/* Button to add the entered heading to the todo list */}
+                <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
+            </div>
         </div>
-      </div>
-      <div className="todo_main">
-        
-      </div>
-    </>
-  );
+        <div className="todo_main">
+            
+        </div>
+        </>
+    );
 };
 
 export default TodoList;
